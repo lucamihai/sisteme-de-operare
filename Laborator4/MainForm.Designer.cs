4a48f1f5e1884402673908dba09686c6ecc057de
lucamihai <luca.mihai97@yahoo.com>
2019-04-01 20:36:14 +0300

    Threads call the same function

---
+++ Laborator4/MainForm.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Laborator4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSuspend1 = new System.Windows.Forms.Button();
+            this.buttonSuspend2 = new System.Windows.Forms.Button();
+            this.buttonSuspend3 = new System.Windows.Forms.Button();
+            this.buttonSuspend4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.buttonStartThreads = new System.Windows.Forms.Button();
+            this.buttonKillThreads = new System.Windows.Forms.Button();
+            this.buttonTimeThread2 = new System.Windows.Forms.Button();
+            this.buttonTimeThread1 = new System.Windows.Forms.Button();
+            this.buttonTimeThread3 = new System.Windows.Forms.Button();
+            this.buttonTimeThread4 = new System.Windows.Forms.Button();
+            this.textBoxTimeData = new System.Windows.Forms.TextBox();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSuspend1
+            // 
+            this.buttonSuspend1.Location = new System.Drawing.Point(336, 78);
+            this.buttonSuspend1.Name = "buttonSuspend1";
+            this.buttonSuspend1.Size = new System.Drawing.Size(75, 23);
+            this.buttonSuspend1.TabIndex = 0;
+            this.buttonSuspend1.Text = "Suspend";
+            this.buttonSuspend1.UseVisualStyleBackColor = true;
+            this.buttonSuspend1.Click += new System.EventHandler(this.SuspendThread1);
+            // 
+            // buttonSuspend2
+            // 
+            this.buttonSuspend2.Location = new System.Drawing.Point(336, 107);
+            this.buttonSuspend2.Name = "buttonSuspend2";
+            this.buttonSuspend2.Size = new System.Drawing.Size(75, 23);
+            this.buttonSuspend2.TabIndex = 1;
+            this.buttonSuspend2.Text = "Suspend";
+            this.buttonSuspend2.UseVisualStyleBackColor = true;
+            this.buttonSuspend2.Click += new System.EventHandler(this.SuspendThread2);
+            // 
+            // buttonSuspend3
+            // 
+            this.buttonSuspend3.Location = new System.Drawing.Point(336, 136);
+            this.buttonSuspend3.Name = "buttonSuspend3";
+            this.buttonSuspend3.Size = new System.Drawing.Size(75, 23);
+            this.buttonSuspend3.TabIndex = 2;
+            this.buttonSuspend3.Text = "Suspend";
+            this.buttonSuspend3.UseVisualStyleBackColor = true;
+            this.buttonSuspend3.Click += new System.EventHandler(this.SuspendThread3);
+            // 
+            // buttonSuspend4
+            // 
+            this.buttonSuspend4.Location = new System.Drawing.Point(336, 165);
+            this.buttonSuspend4.Name = "buttonSuspend4";
+            this.buttonSuspend4.Size = new System.Drawing.Size(75, 23);
+            this.buttonSuspend4.TabIndex = 3;
+            this.buttonSuspend4.Text = "Suspend";
+            this.buttonSuspend4.UseVisualStyleBackColor = true;
+            this.buttonSuspend4.Click += new System.EventHandler(this.SuspendThread4);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(84, 78);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(218, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(84, 107);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(218, 23);
+            this.progressBar2.TabIndex = 5;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(84, 136);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(218, 23);
+            this.progressBar3.TabIndex = 6;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(84, 165);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(218, 23);
+            this.progressBar4.TabIndex = 7;
+            // 
+            // buttonStartThreads
+            // 
+            this.buttonStartThreads.Location = new System.Drawing.Point(84, 391);
+            this.buttonStartThreads.Name = "buttonStartThreads";
+            this.buttonStartThreads.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartThreads.TabIndex = 8;
+            this.buttonStartThreads.Text = "Begin";
+            this.buttonStartThreads.UseVisualStyleBackColor = true;
+            this.buttonStartThreads.Click += new System.EventHandler(this.StartThreads);
+            // 
+            // buttonKillThreads
+            // 
+            this.buttonKillThreads.Location = new System.Drawing.Point(234, 391);
+            this.buttonKillThreads.Name = "buttonKillThreads";
+            this.buttonKillThreads.Size = new System.Drawing.Size(75, 23);
+            this.buttonKillThreads.TabIndex = 9;
+            this.buttonKillThreads.Text = "Kill the mall";
+            this.buttonKillThreads.UseVisualStyleBackColor = true;
+            this.buttonKillThreads.Click += new System.EventHandler(this.KillThreads);
+            // 
+            // buttonTimeThread2
+            // 
+            this.buttonTimeThread2.Location = new System.Drawing.Point(417, 107);
+            this.buttonTimeThread2.Name = "buttonTimeThread2";
+            this.buttonTimeThread2.Size = new System.Drawing.Size(75, 23);
+            this.buttonTimeThread2.TabIndex = 10;
+            this.buttonTimeThread2.Text = "Time";
+            this.buttonTimeThread2.UseVisualStyleBackColor = true;
+            this.buttonTimeThread2.Click += new System.EventHandler(this.TimeThread2);
+            // 
+            // buttonTimeThread1
+            // 
+            this.buttonTimeThread1.Location = new System.Drawing.Point(417, 78);
+            this.buttonTimeThread1.Name = "buttonTimeThread1";
+            this.buttonTimeThread1.Size = new System.Drawing.Size(75, 23);
+            this.buttonTimeThread1.TabIndex = 10;
+            this.buttonTimeThread1.Text = "Time";
+            this.buttonTimeThread1.UseVisualStyleBackColor = true;
+            this.buttonTimeThread1.Click += new System.EventHandler(this.TimeThread1);
+            // 
+            // buttonTimeThread3
+            // 
+            this.buttonTimeThread3.Location = new System.Drawing.Point(417, 136);
+            this.buttonTimeThread3.Name = "buttonTimeThread3";
+            this.buttonTimeThread3.Size = new System.Drawing.Size(75, 23);
+            this.buttonTimeThread3.TabIndex = 11;
+            this.buttonTimeThread3.Text = "Time";
+            this.buttonTimeThread3.UseVisualStyleBackColor = true;
+            this.buttonTimeThread3.Click += new System.EventHandler(this.TimeThread3);
+            // 
+            // buttonTimeThread4
+            // 
+            this.buttonTimeThread4.Location = new System.Drawing.Point(417, 165);
+            this.buttonTimeThread4.Name = "buttonTimeThread4";
+            this.buttonTimeThread4.Size = new System.Drawing.Size(75, 23);
+            this.buttonTimeThread4.TabIndex = 12;
+            this.buttonTimeThread4.Text = "Time";
+            this.buttonTimeThread4.UseVisualStyleBackColor = true;
+            this.buttonTimeThread4.Click += new System.EventHandler(this.TimeThread4);
+            // 
+            // textBoxTimeData
+            // 
+            this.textBoxTimeData.Location = new System.Drawing.Point(475, 265);
+            this.textBoxTimeData.Multiline = true;
+            this.textBoxTimeData.Name = "textBoxTimeData";
+            this.textBoxTimeData.Size = new System.Drawing.Size(231, 149);
+            this.textBoxTimeData.TabIndex = 13;
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(472, 249);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(30, 13);
+            this.labelTime.TabIndex = 14;
+            this.labelTime.Text = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.textBoxTimeData);
+            this.Controls.Add(this.buttonTimeThread4);
+            this.Controls.Add(this.buttonTimeThread3);
+            this.Controls.Add(this.buttonTimeThread1);
+            this.Controls.Add(this.buttonTimeThread2);
+            this.Controls.Add(this.buttonKillThreads);
+            this.Controls.Add(this.buttonStartThreads);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonSuspend4);
+            this.Controls.Add(this.buttonSuspend3);
+            this.Controls.Add(this.buttonSuspend2);
+            this.Controls.Add(this.buttonSuspend1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSuspend1;
+        private System.Windows.Forms.Button buttonSuspend2;
+        private System.Windows.Forms.Button buttonSuspend3;
+        private System.Windows.Forms.Button buttonSuspend4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.Button buttonStartThreads;
+        private System.Windows.Forms.Button buttonKillThreads;
+        private System.Windows.Forms.Button buttonTimeThread2;
+        private System.Windows.Forms.Button buttonTimeThread1;
+        private System.Windows.Forms.Button buttonTimeThread3;
+        private System.Windows.Forms.Button buttonTimeThread4;
+        private System.Windows.Forms.TextBox textBoxTimeData;
+        private System.Windows.Forms.Label labelTime;
+    }
+}
+
---
+++ Laborator4/MainForm.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Common;
+
+namespace Laborator4
+{
+    public partial class MainForm : Form
+    {
+        private bool Thread1Running
+        {
+            get => buttonSuspend1.Text == "Suspend";
+            set
+            {
+                if (value)
+                {
+                    WinApiClass.ResumeThread((IntPtr)threadHandle1);
+                    buttonSuspend1.Text = "Suspend";
+                }
+                else
+                {
+                    WinApiClass.SuspendThread((IntPtr)threadHandle1);
+                    buttonSuspend1.Text = "Resume";
+                }
+            }
+        }
+
+        private bool Thread2Running
+        {
+            get => buttonSuspend2.Text == "Suspend";
+            set
+            {
+                if (value)
+                {
+                    WinApiClass.ResumeThread((IntPtr)threadHandle2);
+                    buttonSuspend2.Text = "Suspend";
+                }
+                else
+                {
+                    WinApiClass.SuspendThread((IntPtr)threadHandle2);
+                    buttonSuspend2.Text = "Resume";
+                }
+            }
+        }
+
+        private bool Thread3Running
+        {
+            get => buttonSuspend3.Text == "Suspend";
+            set
+            {
+                if (value)
+                {
+                    WinApiClass.ResumeThread((IntPtr)threadHandle3);
+                    buttonSuspend3.Text = "Suspend";
+                }
+                else
+                {
+                    WinApiClass.SuspendThread((IntPtr)threadHandle3);
+                    buttonSuspend3.Text = "Resume";
+                }
+            }
+        }
+
+        private bool Thread4Running
+        {
+            get => buttonSuspend4.Text == "Suspend";
+            set
+            {
+                if (value)
+                {
+                    WinApiClass.ResumeThread((IntPtr)threadHandle4);
+                    buttonSuspend4.Text = "Suspend";
+                }
+                else
+                {
+                    WinApiClass.SuspendThread((IntPtr)threadHandle4);
+                    buttonSuspend4.Text = "Resume";
+                }
+            }
+        }
+
+        private uint threadId1;
+        private uint threadId2;
+        private uint threadId3;
+        private uint threadId4;
+
+        private uint threadHandle1;
+        private uint threadHandle2;
+        private uint threadHandle3;
+        private uint threadHandle4;
+
+        private List<ProgressBar> progressBars;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeProgressBarLimits();
+            InitializeProgressBarList();
+
+            buttonSuspend1.Enabled = false;
+            buttonSuspend2.Enabled = false;
+            buttonSuspend3.Enabled = false;
+            buttonSuspend4.Enabled = false;
+            buttonKillThreads.Enabled = false;
+        }
+
+        private void InitializeProgressBarLimits()
+        {
+            progressBar1.Minimum = Constants.ProgressBarMinimumValue;
+            progressBar1.Maximum = Constants.ProgressBarMaximumValue;
+
+            progressBar2.Minimum = Constants.ProgressBarMinimumValue;
+            progressBar2.Maximum = Constants.ProgressBarMaximumValue;
+
+            progressBar3.Minimum = Constants.ProgressBarMinimumValue;
+            progressBar3.Maximum = Constants.ProgressBarMaximumValue;
+
+            progressBar4.Minimum = Constants.ProgressBarMinimumValue;
+            progressBar4.Maximum = Constants.ProgressBarMaximumValue;
+        }
+
+        private void InitializeThreads()
+        {
+            threadHandle1 = WinApiClass.CreateThread(
+                IntPtr.Zero,
+                32000,
+                (WinApiClass.LPTHREAD_START_ROUTINE)IncrementProgressBar,
+                (IntPtr)GCHandle.Alloc(0),
+                0,
+                out threadId1
+            );
+
+            threadHandle2 = WinApiClass.CreateThread(
+                IntPtr.Zero,
+                32000,
+                (WinApiClass.LPTHREAD_START_ROUTINE)IncrementProgressBar,
+                (IntPtr)GCHandle.Alloc(1),
+                0,
+                out threadId2
+            );
+
+            threadHandle3 = WinApiClass.CreateThread(
+                IntPtr.Zero,
+                32000,
+                (WinApiClass.LPTHREAD_START_ROUTINE)IncrementProgressBar,
+                (IntPtr)GCHandle.Alloc(2),
+                0,
+                out threadId3
+            );
+
+            threadHandle4 = WinApiClass.CreateThread(
+                IntPtr.Zero,
+                32000,
+                (WinApiClass.LPTHREAD_START_ROUTINE)IncrementProgressBar,
+                (IntPtr)GCHandle.Alloc(3),
+                0,
+                out threadId4
+            );
+        }
+
+        private void InitializeProgressBarList()
+        {
+            progressBars = new List<ProgressBar>
+            {
+                progressBar1,
+                progressBar2,
+                progressBar3,
+                progressBar4
+            };
+        }
+
+        private uint IncrementProgressBar(IntPtr lpParam)
+        {
+            var paramHandle = (GCHandle)lpParam;
+            var progressBarIndex = (int)paramHandle.Target;
+            var progressBar = progressBars[progressBarIndex];
+
+            while (progressBar.Value < Constants.ProgressBarMaximumValue)
+            {
+                var methodInvoker = new MethodInvoker(() => progressBar.Value++);
+                progressBar.Invoke(methodInvoker);
+            }
+
+            return 0;
+        }
+
+        private void SuspendThread1(object sender, EventArgs e)
+        {
+            Thread1Running = !Thread1Running;
+        }
+
+        private void SuspendThread2(object sender, EventArgs e)
+        {
+            Thread2Running = !Thread2Running;
+        }
+
+        private void SuspendThread3(object sender, EventArgs e)
+        {
+            Thread3Running = !Thread3Running;
+        }
+
+        private void SuspendThread4(object sender, EventArgs e)
+        {
+            Thread4Running = !Thread4Running;
+        }
+
+        private void StartThreads(object sender, EventArgs e)
+        {
+            InitializeThreads();
+
+            Thread1Running = true;
+            Thread2Running = true;
+            Thread3Running = true;
+            Thread4Running = true;
+
+            buttonSuspend1.Enabled = true;
+            buttonSuspend2.Enabled = true;
+            buttonSuspend3.Enabled = true;
+            buttonSuspend4.Enabled = true;
+            buttonKillThreads.Enabled = true;
+
+            WinApiClass.SetThreadPriority((IntPtr)threadId1, WinApiClass.ThreadPriority.THREAD_PRIORITY_IDLE);
+            WinApiClass.SetThreadPriority((IntPtr)threadId2, WinApiClass.ThreadPriority.THREAD_PRIORITY_BELOW_NORMAL);
+            WinApiClass.SetThreadPriority((IntPtr)threadId3, WinApiClass.ThreadPriority.THREAD_PRIORITY_NORMAL);
+            WinApiClass.SetThreadPriority((IntPtr)threadId4, WinApiClass.ThreadPriority.THREAD_PRIORITY_ABOVE_NORMAL);
+
+            buttonStartThreads.Enabled = false;
+        }
+
+        private void KillThreads(object sender, EventArgs e)
+        {
+            WinApiClass.TerminateThread(threadHandle1, 0);
+            WinApiClass.TerminateThread(threadHandle2, 0);
+            WinApiClass.TerminateThread(threadHandle3, 0);
+            WinApiClass.TerminateThread(threadHandle4, 0);
+
+            buttonSuspend1.Enabled = false;
+            buttonSuspend2.Enabled = false;
+            buttonSuspend3.Enabled = false;
+            buttonSuspend4.Enabled = false;
+        }
+
+        private void TimeThread1(object senders, EventArgs e)
+        {
+            GetAndDisplayValuesForThread(threadHandle1);
+        }
+
+        private void TimeThread2(object sender, EventArgs e)
+        {
+            GetAndDisplayValuesForThread(threadHandle2);
+        }
+
+        private void TimeThread3(object sender, EventArgs e)
+        {
+            GetAndDisplayValuesForThread(threadHandle3);
+        }
+
+        private void TimeThread4(object sender, EventArgs e)
+        {
+            GetAndDisplayValuesForThread(threadHandle4);
+        }
+
+        private void GetAndDisplayValuesForThread(uint threadHandle)
+        {
+            var lpCreationTime = new WinApiClass.FILETIME();
+            var lpExitTime = new WinApiClass.FILETIME();
+            var lpKernelTime = new WinApiClass.FILETIME();
+            var lpUserTime = new WinApiClass.FILETIME();
+
+            WinApiClass.GetThreadTimes(
+                (IntPtr) threadHandle,
+                out lpCreationTime,
+                out lpExitTime, 
+                out lpKernelTime,
+                out lpUserTime
+            );
+
+            var lpSystemTimeCreationTime = new WinApiClass.SYSTEMTIME();
+            var lpSystemTimeExitTime = new WinApiClass.SYSTEMTIME();
+            var lpSystemTimeKernelTime = new WinApiClass.SYSTEMTIME();
+            var lpSystemTimeUserTime = new WinApiClass.SYSTEMTIME();
+
+            WinApiClass.FileTimeToSystemTime(ref lpCreationTime, out lpSystemTimeCreationTime);
+            WinApiClass.FileTimeToSystemTime(ref lpExitTime, out lpSystemTimeExitTime);
+            WinApiClass.FileTimeToSystemTime(ref lpKernelTime, out lpSystemTimeKernelTime);
+            WinApiClass.FileTimeToSystemTime(ref lpUserTime, out lpSystemTimeUserTime);
+
+            var systemCreationTimeString = GetStringRepresentationForSystemTime(lpSystemTimeCreationTime);
+            var systemExitTimeString = GetStringRepresentationForSystemTime(lpSystemTimeExitTime);
+
+            textBoxTimeData.Text = $"Creation time: {systemCreationTimeString}\r\nExit time: {systemExitTimeString}\r\n";
+            textBoxTimeData.Text += $"Kernel time: {lpSystemTimeKernelTime.Milliseconds} ms\r\n";
+            textBoxTimeData.Text += $"User time: {lpSystemTimeUserTime.Milliseconds} ms";
+        }
+
+        private string GetStringRepresentationForSystemTime(WinApiClass.SYSTEMTIME systemTime)
+        {
+            return
+                $"{systemTime.Day}.{systemTime.Month}.{systemTime.Year} {systemTime.Hour}:{systemTime.Minute}:{systemTime.Milliseconds}";
+        }
+    }
+}
